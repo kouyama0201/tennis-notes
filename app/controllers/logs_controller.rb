f58a49c82cba337651d1b2b0ea4e412e01cef7d4
kouyama0201 <kami19900201@gmail.com>
2019-10-19 22:33:52 +0900

    投稿機能(仮)の実装

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,6 +1,19 @@
 class LogsController < ApplicationController
 
   def index
+    @log = Log.page(params[:page]).per(5).order("created_at DESC")
   end
 
+  def new
+    @log = Log.new
+  end
+
+  def create
+    Log.create(log_params)
+  end
+
+  private
+  def log_params
+    params.require(:log).permit(:serve, :smash, :volley, :stroke, :game, :text, :image)
+  end
 end
