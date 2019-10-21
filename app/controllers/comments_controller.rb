33ffa0089800f691ac7ad5176b61f8f9f148c306
kouyama0201 <kami19900201@gmail.com>
2019-10-21 17:59:49 +0900

    コメント削除機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
+  def destroy
+    @log = Log.find(params[:log_id])
+    @comment = @log.comments.find(params[:id])
+    @comment.destroy
+    redirect_to log_path(@log)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(log_id: params[:log_id], user_id: current_user.id)
