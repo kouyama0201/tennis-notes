77c7d0172451e2c844d21bf39227466737bedf8b
kouyama0201 <kami19900201@gmail.com>
2019-11-16 20:34:09 +0900

    フォロー機能の基礎の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user
 
   def show
-    user = User.find(params[:id])
-    @name = user.name
-    @log = user.logs.page(params[:page]).per(6).order("created_at DESC")
+    @name = @user.name
+    @log = @user.logs.page(params[:page]).per(6).order("created_at DESC")
+  end
+
+  def following
+    @title = "フォロー"
+    @users = @user.followings
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "フォロワー"
+    @users = @user.followers
+    render 'show_follow'
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 end
