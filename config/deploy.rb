7b4f9375c3000f8a94d2e523b704d4d066be7d92
kouyama0201 <kami19900201@gmail.com>
2020-02-07 22:43:59 +0900

    自動デプロイの為の記述修正

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+lock '3.11.2'
+
+set :application, 'tennis-notes'
+
+set :repo_url,  'git@github.com:kouyama0201/tennis-notes.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.3'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/tennis-notes.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
