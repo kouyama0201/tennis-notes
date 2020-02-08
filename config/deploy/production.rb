ab97cab42b4cf9bbd2383ab2b4881d27510c4c77
kouyama0201 <kami19900201@gmail.com>
2020-02-08 21:34:59 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ server "3.115.27.147", user: "kouyama0201", roles: %w{web app db}, port: 22,
 
   ssh_options: {
     user: 'kouyama0201',
-    keys: [File.expand_path('~/.ssh/tennis-notes_key_rsa')],
+    keys: %w(~/.ssh/tennis-notes_key_rsa),
     forward_agent: true,
   }
 
