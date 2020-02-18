a72d64a038e339ba3bf1320d04b35d6d2853a9f8
kouyama0201 <kami19900201@gmail.com>
2020-02-18 21:10:42 +0900

    validationの記述を追加

--- app/models/log.rb
+++ app/models/log.rb
@@ -4,6 +4,13 @@ class Log < ApplicationRecord
   has_many :likes, dependent: :destroy
   mount_uploader :image, ImageUploader
 
+  validates :practice_day, presence: true
+  validates :serve, :smash, :volley, :stroke, :game,
+            presence: true, 
+            numericality: { only_integer: true,
+                            greater_than_or_equal_to: 0,
+                            less_than_or_equal_to: 1440 }
+
   def like_user(user_id)
     likes.find_by(user_id: user_id)
   end
