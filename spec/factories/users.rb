9dfb569917fa5a40d876e1b2d15dd82ad5368af9
kouyama0201 <kami19900201@gmail.com>
2020-02-23 12:38:50 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
-
   factory :user do
-    name                  {"abc"}
-    email                 {"abc@gmail.com"}
-    password              {"000000"}
-    password_confirmation {"000000"}
+    name                  { "abc" }
+    email                 { "abc@gmail.com" }
+    password              { "000000" }
+    password_confirmation { "000000" }
   end
-
 end
