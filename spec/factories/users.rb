53b4be64d97dd26f1e87436cefcade04f655e083
kouyama0201 <kami19900201@gmail.com>
2020-02-23 12:34:18 +0900

    FactoryBotのuserの設定

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :user do
+    name                  {"abc"}
+    email                 {"abc@gmail.com"}
+    password              {"000000"}
+    password_confirmation {"000000"}
+  end
+
+end
