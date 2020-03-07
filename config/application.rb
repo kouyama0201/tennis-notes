37996193930d12794bfdda59312bacb8fbb155b4
kouyama0201 <kami19900201@gmail.com>
2020-03-08 01:44:19 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module TennisNotes
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+    config.assets.unknown_asset_fallback = true
   end
 end
