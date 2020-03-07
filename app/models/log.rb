4da8d13618250149141e9eb956ac28ed8d3830ab
kouyama0201 <kami19900201@gmail.com>
2020-03-07 14:39:54 +0900

    不要なvalidateの削除

--- app/models/log.rb
+++ app/models/log.rb
@@ -7,9 +7,7 @@ class Log < ApplicationRecord
   validates :practice_day, presence: true
   validates :serve, :smash, :volley, :stroke, :game,
             presence: true,
-            numericality: { only_integer: true,
-                            greater_than_or_equal_to: 0,
-                            less_than_or_equal_to: 1440 }
+            numericality: { only_integer: true }
 
   def like_user(user_id)
     likes.find_by(user_id: user_id)
