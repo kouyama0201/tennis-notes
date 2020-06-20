134536b120f5bebb743f489148121e224b586a48
kouyama0201 <kami19900201@gmail.com>
2020-06-20 20:54:42 +0900

    update: 文面とcolorの変更

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,22 +1,22 @@
 $(window).on('load', function () { // 各ページでのサイドバーのボタン色変え
   if (document.URL.match("logs/new")) {
-    $(".new").css("background-color", "#EEEEEE");
+    $(".new").css("background-color", "#CCCCCC");
   } else if (document.URL.match(/\/logs\/\d+\/edit/)) {
   } else if (document.URL.match(/\/users\/\d+\/like/)) {
-    $(".like").css("background-color", "#EEEEEE");
+    $(".like").css("background-color", "#CCCCCC");
   } else if (document.URL.match(/\/users\/\d+\/following/)) {
-    $(".following").css("background-color", "#EEEEEE");
+    $(".following").css("background-color", "#CCCCCC");
   } else if (document.URL.match(/\/users\/\d+\/followers/)) {
-    $(".followers").css("background-color", "#EEEEEE");
+    $(".followers").css("background-color", "#CCCCCC");
   } else if (document.URL.match(/\/users\/\d/)) {
-    $(".user").css("background-color", "#EEEEEE");
+    $(".user").css("background-color", "#CCCCCC");
   } else if (document.URL.match("users/edit")) {
-    $(".user-edit").css("background-color", "#EEEEEE");
+    $(".user-edit").css("background-color", "#CCCCCC");
   } else if (document.URL.match("users")) {
-    $(".users").css("background-color", "#EEEEEE");
+    $(".users").css("background-color", "#CCCCCC");
   } else if (document.URL.match("invites")) {
-    $(".invites").css("background-color", "#EEEEEE");
+    $(".invites").css("background-color", "#CCCCCC");
   } else {
-    $(".top").css("background-color", "#EEEEEE");
+    $(".top").css("background-color", "#CCCCCC");
   }
 });
